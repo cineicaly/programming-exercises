1bd99a802c8a0a08bc06e8071c0e2457753d74e9
cineicaly <darius.cipleu@student.upt.ro>
2026-06-27 21:28:18 +0300

    solved merge-two-sorted-lists + removed redundant code in print_list()

--- LinkedListExercises/merge-two-sorted-lists/main.c
+++ LinkedListExercises/merge-two-sorted-lists/main.c
@@ -17,23 +17,43 @@ void *safe_malloc(size_t n)
     return p;
 }
 
+void print_list(struct ListNode* ll)
+{
+    while (ll) {
+        printf("%d", ll->val);
+        ll = ll->next;
+    }
+    printf("\n");
+}
+
 struct ListNode *mergeTwoLists(struct ListNode *L1, struct ListNode *L2)
 {
-    if (L1 == NULL && L2 != NULL) {
-        return L2;
-    }
-    if (L1 != NULL && L2 == NULL) {
-        return L1;
-    }
-
     struct ListNode dummy;
     dummy.next = NULL;
     struct ListNode *curr = &dummy;
 
-    while (L1 || L2) {
-
+    while (L1 && L2) {
+        if (L1->val <= L2->val) {
+            curr->next = L1;
+            L1 = L1->next;
+        } else {
+            curr->next = L2;
+            L2 = L2->next;
+        }
+        curr = curr->next;
     }
+
+    curr->next = L1 ? L1 : L2;
     return dummy.next;
+}
+
+void free_ll(struct ListNode* ll) {
+    struct ListNode* temp;
+    while (ll) {
+        temp = ll;
+        ll = ll->next;
+        free(temp);
+    }
 }
 
 int main(void)
@@ -43,9 +63,28 @@ int main(void)
     struct ListNode *A_second = safe_malloc(sizeof(struct ListNode));
     struct ListNode *A_third = safe_malloc(sizeof(struct ListNode));
 
+    A_head->val = 1;
+    A_head->next = A_second;
+    A_second->val = 2;
+    A_second->next = A_third;
+    A_third->val = 4;
+    A_third->next = NULL;
+
     struct ListNode *B_head = safe_malloc(sizeof(struct ListNode));
     struct ListNode *B_second = safe_malloc(sizeof(struct ListNode));
     struct ListNode *B_third = safe_malloc(sizeof(struct ListNode));
+
+    B_head->val = 1;
+    B_head->next = B_second;
+    B_second->val = 3;
+    B_second->next = B_third;
+    B_third->val = 4;
+    B_third->next = NULL;
+
+    struct ListNode *merged = mergeTwoLists(A_head, B_head);
+    print_list(merged);
+
+    free_ll(merged);
 
     return 0;
 }
